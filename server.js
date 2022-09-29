41d7e9869aabb9ddd83377989f635ad34c94ef66
magnetoasher <magnetoasher@gmail.com>
2022-09-28 19:11:36 -0700

    feat:fixed server.js

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ app.post('/api/subscribe', async (req, res) => {
 const PORT = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/build'));
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}.`);
 });
